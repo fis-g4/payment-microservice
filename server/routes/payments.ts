c8b412c49d36d3586665fe69e6a47e334ef5f224
Youssef <youssefldpro@gmail.com>
2023-11-30 19:55:54 +0100

    feat: add user plan services

--- server/routes/payments.ts
+++ server/routes/payments.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import { Payment } from '../db/models/payment'
+import { getCourseById } from '../mocks/courses.mock'
 import { getPlanById } from '../mocks/plans.mock'
 import { getUserById } from '../mocks/users.mock'
 
@@ -42,11 +43,28 @@ router.post(
         const { courseId, userId } = req.params
 
         // Step 1: Fetch the course from the database
+        const course = getCourseById(courseId)
+        if (!course) {
+            return res.status(404).json({ error: 'Course not found' })
+        }
         // Step 2: Fetch the user from the database
+        const user = getUserById(userId)
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' })
+        }
         // Step 3: Create a payment record
+        const payment = Payment.build({
+            amount: course.price,
+            currency: course.currency,
+            referenceId: course.id,
+            referenceType: 'course',
+            status: 'pending',
+            userId: user.id,
+        })
         // Step 4: Return the payment record
+        await payment.save()
 
-        return res.status(200).json({ courseId, userId })
+        return res.status(200).json({ payment })
     }
 )
 
@@ -68,10 +86,17 @@ router.get('/users/:userId', async (req: Request, res: Response) => {
     const { userId } = req.params
 
     // Step 1: Fetch the user from the database
+    const user = getUserById(userId)
+    if (!user) {
+        return res.status(404).json({ error: 'User not found' })
+    }
     // Step 2: Fetch the payment records for the user
+    const payments = await Payment.find({ where: { userId: user.id } })
+    if (!payments) {
+        return res.status(404).json({ error: 'Payments not found' })
+    }
     // Step 3: Return the payment records
-
-    return res.status(200).json({ userId })
+    return res.status(200).json({ payments })
 })
 
 export default router

ea1ada2b6166123d841b2ed2c1ff7ca9e59554b9
Youssef Lakouifat <youssefldpro@gmail.com>
2024-01-22 23:28:40 +0100

    feat: add courses connection

---
+++ server/service/user.ts
@@ -0,0 +1,105 @@
+import axios from 'axios'
+
+export class UserService {
+    private apiUrl: string | undefined
+    private username: string | undefined
+    private password: string | undefined
+    private token: string | undefined
+
+    constructor() {
+        this.apiUrl = process.env.USER_SERVICE_BASE_URL
+        this.username = process.env.USER_SERVICE_USERNAME
+        this.password = process.env.USER_SERVICE_PASSWORD
+    }
+
+    async login() {
+        try {
+            console.log('Lets login')
+            const response = await axios.post(`${this.apiUrl}/login`, {
+                username: this.username,
+                password: this.password,
+            })
+
+            if (response && response.data && response.data.data) {
+                this.token = response.data.data.token
+                console.log('Login successful. Token obtained.')
+            } else {
+                console.log('Login failed. Token not obtained.')
+            }
+        } catch (error: any) {
+            console.error('Error during login:', error.message)
+        }
+    }
+
+    async getUserByUsername(userName: string) {
+        try {
+            if (!this.token) {
+                console.error('Please login first.')
+                return null
+            }
+
+            const response = await axios.get(`${this.apiUrl}/${userName}`, {
+                headers: {
+                    Authorization: `Bearer ${this.token}`,
+                },
+            })
+
+            if (response && response.data) {
+                console.log('User details obtained:', response.data)
+                return response.data as UserReponse
+            } else {
+                console.log('User details not found.')
+                return null
+            }
+        } catch (error: any) {
+            console.error('Error getting user details:', error.message)
+            return null
+        }
+    }
+
+    async update(userName: string, body: UpdateBody) {
+        try {
+            if (!this.token) {
+                console.error('Please login first.')
+                return null
+            }
+
+            const response = await axios.put(
+                `${this.apiUrl}/users/${userName}`,
+                body,
+                {
+                    headers: {
+                        Authorization: `Bearer ${this.token}`,
+                    },
+                }
+            )
+
+            if (response && response.data) {
+                console.log('User details updated:', response.data)
+                return response.data as UserReponse
+            } else {
+                console.log('User details not updated.')
+                return null
+            }
+        } catch (error: any) {
+            console.error('Error updating user details:', error.message)
+            return null
+        }
+    }
+}
+
+interface UpdateBody {
+    plan: string
+}
+
+interface UserReponse {
+    data: {
+        id: string
+        firstName: string
+        lastName: string
+        userName: string
+        email: string
+        plan: string
+        role: string
+    }
+}
